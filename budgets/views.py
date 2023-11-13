4c9ce98d48273b7dc0132db4d280c8d74e48dc91
tneoeo2 <tneoeo2@gmail.com>
2023-11-14 02:06:44 +0900

    :sparkles:Feat : 예산 설정 수정 api
    - 예산 설정 수정 api
    
    #7

--- budgets/views.py
+++ budgets/views.py
@@ -44,8 +44,10 @@ class SetBudgetView(generics.ListCreateAPIView, mixins.UpdateModelMixin):
     
     def has_budget_for_month(self, user, category, current_month):
         # 해당 월에 동일 카테고리 예산 정보가 있는지 확인
-        return Budgets.objects.filter(user=user, category=category, create_at__month=current_month).exists()
-    
+        try:
+            return Budgets.objects.get(user=user, category=category, create_at__month=current_month)
+        except Budgets.DoesNotExist:
+            return None
     def post(self, request, *args, **kwargs):
         # TODO: user는 같은 월에 하나의 catory-amount를 지정할 수 있다.
         
@@ -59,7 +61,8 @@ class SetBudgetView(generics.ListCreateAPIView, mixins.UpdateModelMixin):
                 current_month = timezone.now().month
                 
                  # 이미 설정한 예산 정보가 있는지 확인
-                if self.has_budget_for_month(user_id, category_id, current_month):
+                instance = self.has_budget_for_month(user_id, category_id, current_month)
+                if instance is not None:
                     return Response({'error': '해당 카테고리에 이미 설정한 예산 정보가 있습니다.'}, status=status.HTTP_400_BAD_REQUEST)
                 
                 serializer = self.serializer_class(data={'user': user_id, 'category': category_id, 'amount': amount})
@@ -72,11 +75,24 @@ class SetBudgetView(generics.ListCreateAPIView, mixins.UpdateModelMixin):
         
       
     def patch(self, request, *args, **kwargs):
+        #예산 정보 수정
+        try:
+            token = request.headers.get("Authorization", "").split(" ")[1]
+            payload = jwt.decode(token, SECRET_KEY, algorithms=ALGORITHM)
+            user_id = payload['user_id']
+            if user_id is not None:
+                category_id = request.data.get('category')
+                amount = request.data.get('amount')
+                current_month = timezone.now().month
+             # 이미 설정한 예산 정보가 있는지 확인
+                instance= self.has_budget_for_month(user_id, category_id, current_month)
+                if instance is not None:
+                    instance.amount = amount
+                    instance.save()
+                    serializer = self.serializer_class(instance)
+                else:
+                    raise ValueError('기존 데이터가 존재하지않습니다')
+                return Response(serializer.data, status=status.HTTP_200_OK)
+        except jwt.InvalidTokenError:
+            return Response({'error' : '권한이 없는 사용자입니다.'}, status.HTTP_401_UNAUTHORIZED) 
         
-        def get_queryset(self):
-            category = self.request.query_params.get('category')
-            amount = self.request.query_params.get('amount')
-            
-        
-        
-        return self.partial_update(request, *args, **kwargs)
